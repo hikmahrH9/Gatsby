e70377cdc9c5a084dc3a7040f61cc55efe16d7fa
hikmahrH9 <hikmahipmsinbor@gmail.com>
2021-03-20 18:13:02 +0800

    My first Gatsby site with GitHub

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,17 +8,17 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Selamat Datang di Pantai Losari</h1>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
+      src="https://thumb.spotlight.id/image/2020/03/18/1bb4a3ccfd34bfdac1de2a82a8af0b38-Pantai-Losari.jpeg"
+      width={500}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
+      alt="pantai losari"
       style={{ marginBottom: `1.45rem` }}
     />
+    <p> Pantai Losari adalah sebuah pantai yang terletak di sebelah barat Kota Makassar, Provinsi Sulawesi Selatan, Indonesia. Pantai ini menjadi tempat bagi warga Makassar untuk menghabiskan waktu pada pagi, sore, dan malam hari menikmati pemandangan matahari tenggelam yang sangat indah (Wikipedia)
+       </p>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
